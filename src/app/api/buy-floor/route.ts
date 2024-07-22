711451d254056e99e016ab1d97d7332a0f781745
rkmonarch <rahulkulkarni5234@gmail.com>
2024-07-22 21:08:46 +0530

    feat: init

---
+++ src/app/api/buy-floor/route.ts
@@ -0,0 +1,118 @@
+import {
+  findCollectionBySlug,
+  getListingsByCollection,
+} from "@/utils/tensor-api";
+import {
+  createBuyNftTransaction,
+  formatTokenAmount,
+  getTotalPrice,
+} from "../../../utils/transactions-utils";
+import { ActionGetResponse } from "@solana/actions";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const collection = req.nextUrl.searchParams.get("collection");
+  // const collectionSlug = c.req.param("collectionSlug");
+  const collectiond = await findCollectionBySlug(collection as string);
+  if (!collectiond) {
+    return NextResponse.json(
+      {
+        message: `Collection ${collection} not found`,
+      },
+      {
+        status: 422,
+      }
+    );
+  }
+  console.log(collectiond);
+  const buyNowPriceNetFees = collectiond.stats.buyNowPriceNetFees
+    ? parseInt(collectiond.stats.buyNowPriceNetFees)
+    : await getListingsByCollection(collectiond.collId).then((resp) =>
+        getTotalPrice(
+          parseInt(resp.mints[0].listing.price),
+          collectiond.sellRoyaltyFeeBPS,
+          resp.mints[0].listing.source
+        )
+      );
+  const numListed = collectiond.stats.numListed;
+  if (numListed < 1) {
+    return NextResponse.json(
+      {
+        icon: collectiond?.imageUri,
+        label: `Not Available`,
+        title: `Buy Floor ${collectiond?.name}`,
+        description: collectiond?.description,
+        disabled: true,
+        error: {
+          message: `Collection has no listed NFTs`,
+        },
+      },
+      {
+        status: 200,
+      }
+    );
+  }
+  const uiPrice = formatTokenAmount(buyNowPriceNetFees / LAMPORTS_PER_SOL);
+  return NextResponse.json({
+    icon: collectiond.imageUri,
+    label: `${uiPrice} SOL`,
+    title: `Buy Floor ${collectiond.name}`,
+    description: collectiond.description,
+  } as ActionGetResponse);
+}
+
+const OPTIONS = GET;
+
+export async function POST(req: NextRequest) {
+  const collection = req.nextUrl.searchParams.get("collection");
+  try {
+    const { account } = await req.json();
+    const collectiond = await findCollectionBySlug(collection as string);
+    if (!collectiond) {
+      return NextResponse.json(
+        {
+          message: `Collection ${collection} not found`,
+        },
+        {
+          status: 422,
+        }
+      );
+    }
+    const floorMint = (await getListingsByCollection(collectiond.collId))
+      .mints[0];
+    if (!floorMint) {
+      return NextResponse.json(
+        {
+          message: `Collection has no listed NFTs`,
+        },
+        {
+          status: 422,
+        }
+      );
+    }
+
+    const transaction = await createBuyNftTransaction(floorMint, account);
+
+    if (!transaction) {
+      throw new Error("Failed to create transaction");
+    }
+
+    return NextResponse.json({
+      transaction: transaction,
+    });
+  } catch (e) {
+    console.error(
+      `Failed to prepare buy floor transaction for ${collection}`,
+      e
+    );
+    return NextResponse.json(
+      {
+        message: `Failed to prepare transaction`,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
---
+++ src/utils/transactions-utils.ts
@@ -0,0 +1,74 @@
+import { connection } from "./connection";
+import { Mint, getNftBuyTransaction } from "./tensor-api";
+
+const SOURCE_TO_FEE_BPS = {
+  TENSORSWAP: 150,
+  TCOMP: 150,
+  MAGICEDEN_V2: 250,
+  default: 150,
+};
+
+export async function createBuyNftTransaction(
+  mint: Mint,
+  buyerAddress: string
+): Promise<string | null> {
+  const blockhash = await connection
+    .getLatestBlockhash({ commitment: "max" })
+    .then((res: any) => res.blockhash);
+
+  const totalPrice = getTotalPrice(
+    parseInt(mint.listing.price, 10),
+    mint.royaltyBps,
+    mint.listing.source
+  );
+  return getNftBuyTransaction({
+    mintAddress: mint.mint,
+    ownerAddress: mint.listing.seller,
+    buyerAddress: buyerAddress,
+    price: totalPrice,
+    latestBlockhash: blockhash,
+  });
+}
+
+export function getTotalPrice(
+  price: number,
+  royaltyBps: number,
+  source: keyof typeof SOURCE_TO_FEE_BPS | string
+): number {
+  const MP_FEE_BPS =
+    source in SOURCE_TO_FEE_BPS
+      ? SOURCE_TO_FEE_BPS[source as keyof typeof SOURCE_TO_FEE_BPS]
+      : SOURCE_TO_FEE_BPS["default"];
+  const royalty = (price * royaltyBps) / 10000;
+  const marketPlaceFee = (price * MP_FEE_BPS) / 10000;
+
+  return price + royalty + marketPlaceFee;
+}
+
+export const USDollar = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 0,
+});
+export function formatTokenAmount(num: number): string {
+  if (num >= 1 && num < 1e3) {
+    return removeTrailingZeros(num.toFixed(2));
+  }
+  if (num >= 1e3 && num < 1e6) {
+    return removeTrailingZeros((num / 1e3).toFixed(1)) + "K";
+  }
+  if (num >= 1e6 && num < 1e9) {
+    return removeTrailingZeros((num / 1e6).toFixed(1)) + "M";
+  }
+  if (num >= 1e9 && num < 1e12) {
+    return removeTrailingZeros((num / 1e9).toFixed(1)) + "B";
+  }
+  if (num >= 1e12) {
+    return removeTrailingZeros((num / 1e12).toFixed(1)) + "T";
+  }
+  return removeTrailingZeros(num.toPrecision(3));
+}
+
+function removeTrailingZeros(value: string): string {
+  return value.replace(/\.?0+$/, "");
+}
